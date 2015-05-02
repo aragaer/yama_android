19356d1b308e5d48c85a0b1ccbbd26420c4afd99
aragaer <aragaer@gmail.com>
2015-05-03 02:21:02 +0300

    Functional tests for memo create activity

---
+++ src/test/java/com/aragaer/yama/CreateActivityTest.java
@@ -0,0 +1,127 @@
+package com.aragaer.yama;
+
+import java.io.*;
+import java.util.*;
+
+import android.content.Context;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class CreateActivityTest {
+
+    @Rule public ActivityTestRule<MainActivity> mActivityRule =
+	    new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    private List<String> readMemosFile() throws Exception {
+	List<String> result = new LinkedList<String>();
+	InputStream file = mActivityRule.getActivity().openFileInput("memo");
+	BufferedReader reader = new BufferedReader(new InputStreamReader(file));
+	while (true) {
+	    String line = reader.readLine();
+	    if (line == null)
+		break;
+	    result.add(line);
+	}
+	file.close();
+	return result;
+    }
+
+    private void writeMemosFile(List<String> lines) throws Exception {
+	OutputStream file = mActivityRule.getActivity().openFileOutput("memo",
+	       Context.MODE_PRIVATE | Context.MODE_APPEND);
+	for (String line : lines) {
+	    file.write(line.getBytes());
+	    file.write("\n".getBytes());
+	}
+	file.close();
+    }
+
+    @After public void tearDown() {
+	mActivityRule.getActivity().deleteFile("memo");
+    }
+
+    @Test public void shouldWriteOneLineMemosToFile() throws Exception {
+	onView(supportsInputMethods()).perform(typeText("Have a cup of Espresso."));
+	onView(withText("Done")).perform(click());
+
+	assertThat(readMemosFile(),
+		   equalTo(Arrays.asList("Have a cup of Espresso.")));
+    }
+
+    @Test public void shouldWriteMultiLineMemosToFile() throws Exception {
+	onView(supportsInputMethods())
+	    .perform(typeText("Have a cup of Espresso.\n"
+			      +"Write a second test."));
+	onView(withText("Done")).perform(click());
+
+	assertThat(readMemosFile(),
+		   equalTo(Arrays.asList("Have a cup of Espresso.",
+					 "Write a second test.")));
+    }
+
+    @Test public void shouldDiscardEmptyLines() throws Exception {
+	onView(supportsInputMethods())
+	    .perform(typeText("\n"
+			      +"Have a cup of Espresso.\n"
+			      +"\n"
+			      +"  \n"
+			      +"\n"
+			      +"Write a second test.\n"
+			      +"\n"));
+	onView(withText("Done")).perform(click());
+
+	assertThat(readMemosFile(),
+		   equalTo(Arrays.asList("Have a cup of Espresso.",
+					 "Write a second test.")));
+    }
+
+    @Test public void shouldTrimStrings() throws Exception {
+	onView(supportsInputMethods())
+	    .perform(typeText("  Have a cup of Espresso.   \n"
+			      +"\n"
+			      +"  \n"
+			      +"  Write a second test.\n"
+			      +"\n"));
+	onView(withText("Done")).perform(click());
+
+	assertThat(readMemosFile(),
+		   equalTo(Arrays.asList("Have a cup of Espresso.",
+					 "Write a second test.")));
+    }
+
+    @Test public void shouldAppendToExistingMemos() throws Exception {
+	writeMemosFile(Arrays.asList("Some initial memo", "Two of them"));
+
+	onView(supportsInputMethods()).perform(typeText("Have a cup of Espresso."));
+	onView(withText("Done")).perform(click());
+
+	assertThat(readMemosFile(),
+		   equalTo(Arrays.asList("Some initial memo",
+					 "Two of them",
+					 "Have a cup of Espresso.")));
+    }
+
+    @Test public void shouldDiscardIfNotSaved() throws Exception {
+	writeMemosFile(Arrays.asList("Some initial memo", "Two of them"));
+
+	onView(supportsInputMethods()).perform(typeText("Have a cup of Espresso."));
+	onView(withText("Cancel")).perform(click());
+
+	assertThat(readMemosFile(),
+		   equalTo(Arrays.asList("Some initial memo",
+					 "Two of them")));
+    }
+}

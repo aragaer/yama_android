6a2fc3b56f8f6bb86ff9db991601df82f093ff9f
Ilya Konovalov <i.konovalov@samsung.com>
2015-06-18 17:01:06 +0300

    Simple orgmode reading and writing works

--- src/main/java/com/aragaer/yama/OrgmodeReaderWriter.java
+++ src/main/java/com/aragaer/yama/OrgmodeReaderWriter.java
@@ -1,20 +1,48 @@
 package com.aragaer.yama;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.StringBuilder;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.SortedSet;
 
 
 public class OrgmodeReaderWriter implements MemoReaderWriter<String> {
 
+    private final MemoFileProvider _fileProvider;
+
+    public OrgmodeReaderWriter(MemoFileProvider fileProvider) {
+	_fileProvider = fileProvider;
+    }
+
     @Override public SortedSet<String> getKeys() {
 	return null;
     }
 
     @Override public void writeMemosForKey(String key, List<? extends Memo> memos) {
+	OutputStream stream = _fileProvider.openFileForWriting(key);
+	new WriteRunner(stream).writeAll(memos);
+	try {
+	    stream.close();
+	} catch (IOException e) {
+	    // oops?
+	}
     }
 
     @Override public List<? extends Memo> readMemosForKey(String key) {
-	return null;
+	LinkedList<_Memo> result = new LinkedList<_Memo>();
+	InputStream stream = _fileProvider.openFileForReading(key);
+	new ReadRunner(stream).fill(result);
+	try {
+	    stream.close();
+	} catch (IOException e) {
+	    // oops?
+	}
+	return result;
     }
 
     @Override public String getDefaultKey() {
@@ -22,5 +50,94 @@ public class OrgmodeReaderWriter implements MemoReaderWriter<String> {
     }
 
     @Override public void dropKey(String key) {
+    }
+
+    private static class _Memo implements Memo {
+
+	private final String _text;
+
+	_Memo(List<String> lines) {
+	    StringBuilder builder = new StringBuilder();
+	    for (String line : lines)
+		builder.append(line).append('\n');
+	    _text = builder.substring(0, builder.length() - 1);
+	}
+
+	@Override public String getText() {
+	    return _text;
+	}
+    }
+
+    private static class WriteRunner {
+
+	private final OutputStream stream;
+
+	WriteRunner(OutputStream stream) {
+	    this.stream = stream;
+	}
+
+	void writeAll(List<? extends Memo> memos) {
+	    try {
+		for (Memo memo : memos)
+		    write(memo);
+	    } catch (IOException e) {
+	    }
+	}
+
+	private void write(Memo memo) throws IOException {
+	    stream.write("* \n".getBytes());
+	    for (String line : memo.getText().split("\n")) {
+		stream.write("  ".getBytes());
+		stream.write(line.getBytes());
+		stream.write('\n');
+	    }
+	}
+    }
+
+    private static class ReadRunner {
+
+	private final BufferedReader reader;
+
+	ReadRunner(InputStream stream) {
+	    reader = new BufferedReader(new InputStreamReader(stream));
+	}
+
+	public void fill(List<_Memo> list) {
+	    while (true) {
+		_Memo memo = read();
+		if (memo == null)
+		    break;
+		list.add(memo);
+	    }
+	}
+
+	private _Memo read() {
+	    try {
+		reader.readLine();
+	    } catch (IOException e) {
+		return null;
+	    }
+	    List<String> lines = new LinkedList<String>();
+	    while (!memoEnded())
+		try {
+		    lines.add(reader.readLine().trim());
+		} catch (IOException e) {
+		    break;
+		}
+	    return lines.isEmpty() ? null : new _Memo(lines);
+	}
+
+	private boolean memoEnded() {
+	    try {
+		if (!reader.ready())
+		    return true;
+		reader.mark(1);
+		boolean result = reader.read() == '*';
+		reader.reset();
+		return result;
+	    } catch (IOException e) {
+		return true;
+	    }
+	}
     }
 }
--- src/test/java/com/aragaer/yama/OrgmodeReaderWriterTest.java
+++ src/test/java/com/aragaer/yama/OrgmodeReaderWriterTest.java
@@ -1,5 +1,12 @@
 package com.aragaer.yama;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.*;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -8,7 +15,56 @@ import static org.junit.Assert.*;
 
 public class OrgmodeReaderWriterTest {
 
-    @Test public void shouldConstruct() {
-	OrgmodeReaderWriter readerWriter = new OrgmodeReaderWriter();
+    TestFileProvider fileProvider;
+    OrgmodeReaderWriter readerWriter;
+
+    @Before public void setUp() {
+	fileProvider = new TestFileProvider();
+	readerWriter = new OrgmodeReaderWriter(fileProvider);
+    }
+
+    @Test public void readMemoFile() {
+	fileProvider.setContents("* \n  memo\n* \n  another memo\n  is multiline\n");
+	List<? extends Memo> memos = readerWriter.readMemosForKey("memo");
+	assertThat(memos.size(), equalTo(2));
+	assertThat(memos.get(0).getText(), equalTo("memo"));
+	assertThat(memos.get(1).getText(), equalTo("another memo\nis multiline"));
+    }
+
+    @Test public void writeMemoFile() {
+	List<TestMemo> memos = Arrays.asList(new TestMemo("memo"), new TestMemo("other\nmulti"));
+	readerWriter.writeMemosForKey("memo", memos);
+	assertThat(fileProvider.opened, equalTo("memo"));
+	String data = fileProvider.written.toString();
+	assertThat(data, equalTo("* \n  memo\n* \n  other\n  multi\n"));
+    }
+
+    private static class TestFileProvider implements MemoFileProvider {
+
+	String opened;
+	String contents;
+	ByteArrayOutputStream written;
+
+	void setContents(String newContents) {
+	    contents = newContents;
+	}
+
+	@Override public List<String> fileList() {
+	    return Arrays.asList(contents == null ? new String[0] : new String[] {"memo"});
+	}
+
+	@Override public InputStream openFileForReading(String name) {
+	    opened = name;
+	    if (contents == null)
+		return null;
+	    else
+		return new ByteArrayInputStream(contents.getBytes());
+	}
+
+	@Override public OutputStream openFileForWriting(String name) {
+	    opened = name;
+	    written = new ByteArrayOutputStream();
+	    return written;
+	}
     }
 }
